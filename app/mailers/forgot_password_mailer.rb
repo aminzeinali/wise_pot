c8fb78822c265e8bb8eff762ade9c3ba9c9f8121
amvaleh <amvaleh@gmail.com>
2015-07-04 14:25:39 +0430

    conflicts resolved

--- app/mailers/forgot_password_mailer.rb
+++ app/mailers/forgot_password_mailer.rb
@@ -1,12 +1,14 @@
 class ForgotPasswordMailer < ApplicationMailer
-default from: "golite.tech.pot@gmail.com"
-def forgot_mail_sender(email)
-	@mis_user = User.find_by_email(email)
-	o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
-	@new_pass = (0...50).map { o[rand(o.length)] }.join
-	@mis_user.password = @new_pass
-	@mis_user.save
-	mail(to: email, subject: 'تغیر رمز عبور')
-end
 
+default from: "amvaleh@gmail.com"
+
+	def forgot_mail_sender(email)
+		@mis_user = User.find_by_email(email)
+		o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+		@new_pass = (0...50).map { o[rand(o.length)] }.join
+		@mis_user.password = @new_pass
+		@mis_user.save
+		puts email.to_s
+		mail(to: email, subject: 'تغیر رمز عبور')
+	end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,6 @@ Rails.application.configure do
   #config.action_mailer.perform_deliveries = true
 
 
-  
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
