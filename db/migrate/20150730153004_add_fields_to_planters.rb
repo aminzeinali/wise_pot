952a6053a6cd7142e1d8678c643b85ea72b67cc1
amvaleh <amvaleh@gmail.com>
2015-09-13 23:56:47 +0430

    planter migration corrected

--- db/migrate/20150730153004_add_fields_to_planters.rb
+++ db/migrate/20150730153004_add_fields_to_planters.rb
@@ -1,7 +1,7 @@
 class AddFieldsToPlanters < ActiveRecord::Migration
   def change
     add_column :planters, :latin_name, :string
-    add_column :planters, :second_name, :strnig
+    add_column :planters, :second_name, :string
     add_column :planters, :germination, :text
     add_column :planters, :plague, :text
     add_column :planters, :keeping, :text
